67703436a9f43ea9efcecada4f476852d0a3b18f
ryosuke-horie <r_horie@sincereed.com>
2025-04-05 22:13:20 +0900

    fix: format

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,7 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	plugins: [
-		reactRouter(),
-		tsconfigPaths(),
-        cloudflare(),
-	],
+	plugins: [reactRouter(), tsconfigPaths(), cloudflare()],
 	build: {
 		target: "esnext",
 		outDir: "build",
